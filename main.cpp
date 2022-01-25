e9a5eb4510143ded9dc53479f8e263d821bc3628
Ak627 <91678317+Ak627@users.noreply.github.com>
2022-01-25 09:53:49 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 #include "SFML/Graphics.hpp"
 using namespace std;
+
+enum DIRECTIONS { LEFT, RIGHT, UP, DOWN }; //left is 0, right is 1, up is 2, down is 3
+
 int main() {
     //game set up (you'll need these lines in every game)
     sf::RenderWindow screen(sf::VideoMode(1920, 1080), "Pacman"); //set up screen
@@ -15,21 +18,31 @@ int main() {
     sf::Sprite wall;
     wall.setTexture(brick);
 
-    int map[30][16] = {
-        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-        1,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,
-        1,0,1,1,1,0,1,1,0,1,0,0,0,0,1,1,
-        1,0,1,0,0,0,1,0,0,0,0,1,1,0,1,0,
-        1,0,0,0,1,1,0,0,1,1,0,1,0,0,0,0,
-        1,1,0,0,0,1,0,0,0,1,0,0,0,1,0,1,
-        1,0,0,1,0,0,0,1,0,1,0,1,0,1,1,1,
-        1,0,1,1,1,0,1,1,0,0,0,1,0,0,0,0,
-        1,0,0,0,0,0,0,0,0,1,0,0,0,1,1,1,
-        1,0,1,1,0,1,1,0,1,1,0,1,1,0,0,0,
-        1,0,0,0,0,0,1,0,0,0,0,1,0,0,1,0,
-        1,1,1,1,1,0,0,0,1,1,0,0,0,1,1,1,
-        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+    int map[30][24] = {
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+        1,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,1,1,1,
+        1,0,1,1,1,0,1,1,0,1,0,0,0,0,1,1,1,0,1,1,0,1,0,1,
+        0,0,1,0,0,0,1,0,0,0,0,1,1,0,1,0,0,0,0,1,0,1,0,0,
+        1,0,1,0,1,1,0,0,1,0,1,1,0,0,0,0,1,1,0,1,0,0,0,1,
+        1,1,0,0,0,1,1,0,1,1,1,1,0,1,0,1,1,1,0,0,0,1,0,1,
+        0,0,0,1,0,0,0,0,0,1,0,0,0,1,1,1,0,0,0,1,0,0,0,0,
+        1,0,1,1,1,0,1,1,0,0,0,1,0,0,0,0,0,1,1,1,0,1,1,1,
+        1,0,0,0,0,0,0,0,0,1,0,0,0,1,1,1,0,1,1,0,0,1,0,1,
+        1,0,1,1,0,1,1,0,1,1,0,1,1,0,0,0,1,1,1,1,0,0,0,1,
+        1,0,0,0,0,0,1,0,0,0,0,1,0,0,1,0,1,0,1,1,0,1,0,1,
+        1,1,1,1,1,0,0,0,1,1,0,0,0,1,1,1,0,0,0,0,0,1,0,1,
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
     };
+    //player set up
+    int xpos = 85;
+    int ypos = 85;
+    int vx = 0;
+    int vy = 0;
+    int radius = 50;
+    sf::CircleShape player(35);
+    player.setFillColor(sf::Color(250, 250, 0)); //using RGB value for color here (hex also works)
+    player.setPosition(xpos, ypos); //top left "corner" of circle (not center!)
+    bool keys[] = { false, false, false, false };
 
     //################### HOLD ONTO YOUR BUTTS, ITS THE GAME LOOP###############################################################
     while (screen.isOpen()) {//keep window open until user shuts it down
@@ -39,17 +52,58 @@ int main() {
             //this checks if the user has clicked the little "x" button in the top right corner
             if (event.type == sf::Event::EventType::Closed)
                 screen.close();
+            //KEYBOARD INPUT 
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+                keys[LEFT] = true;
+            }
+            else keys[LEFT] = false;
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+                keys[RIGHT] = true;
+            }
+            else keys[RIGHT] = false;
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+                keys[DOWN] = true;
+            }
+            else keys[DOWN] = false;
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+                keys[UP] = true;
+            }
+            else keys[UP] = false;
 
         }//end event loop---------------------------------------------------------------
+           //move Mr. Pac
+        if (keys[LEFT] == true)
+            vx = -5;
+        else if (keys[RIGHT] == true)
+            vx = 5;
+        else
+            vx = 0;
+        if (keys[DOWN] == true)
+            vy = 5;
+        else if (keys[UP] == true)
+            vy = -5;
+        else
+            vy = 0;
+        
+        xpos += vx;
+        ypos += vy;
+        player.setPosition(xpos, ypos);
+
+        if (vx > 0 && map[(ypos) / 80][(xpos + radius * 2 + 3) / 80] == 1)
+            vx = 0;
                 //render section-----------------------------------------
         screen.clear(); //wipes screen, without this things smear
         for (int rows = 0; rows < 30; rows++)
-            for (int cols = 0; cols < 15; cols++) {
+            for (int cols = 0; cols < 24; cols++) {
                 if (map[rows][cols] == 1) {
                     wall.setPosition(cols * 80, rows * 80);
                     screen.draw(wall);
                 }
             }
+        screen.draw(player);
         screen.display(); //flips memory drawings onto screen
 
     }//######################## end game loop ###################################################################################
